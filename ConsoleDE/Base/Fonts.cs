2a3e8b630e7537ac88078a0fbc549ecaaa5bc2a8
jvyden <jvyden@jvyden.xyz>
2022-09-20 19:13:13 -0400

    Window hiding basic implementation

--- ConsoleDE/Base/Fonts.cs
+++ ConsoleDE/Base/Fonts.cs
@@ -2,7 +2,7 @@ using SixLabors.Fonts;
 
 namespace ConsoleDE.Base {
     public static class Fonts {
-        public static readonly Font DefaultFont = new("Comic Sans", FontStyle.Regular);
+        public static readonly Font DefaultFont = new("Ubuntu", FontStyle.Regular);
 
         public static void LoadContent() {
             DefaultFont.LoadContent();
--- ConsoleDE/Screens/MainScreen.cs
+++ ConsoleDE/Screens/MainScreen.cs
@@ -1,19 +1,41 @@
 using System.Numerics;
+using System.Threading;
 using ConsoleDE.Base;
+using Furball.Engine;
 using Furball.Engine.Engine.Graphics.Drawables.UiElements;
+using Furball.Engine.Engine.Input.Events;
+using Furball.Vixie;
+using Furball.Vixie.Backends.Shared.Backends;
 using Screen = Furball.Engine.Engine.Screen;
+using Task = System.Threading.Tasks.Task;
 
 namespace ConsoleDE.Screens {
     public class MainScreen : Screen {
+        private DrawableButton hideButton;
+        
         public override void Initialize() {
-            this.Manager.Add(new DrawableButton(new Vector2(20, 20),
+            this.Manager.Add(this.hideButton = new DrawableButton(new Vector2(20, 20),
                 Fonts.DefaultFont,
                 24,
-                "buton",
+                "Hide for 5 seconds",
                 Palettes.Light.MainColor,
                 Palettes.Light.TextColor,
                 Palettes.Light.SecondaryColor,
-                new Vector2(100, 50)));
+                new Vector2(250, 50)));
+            
+            this.hideButton.OnClick += hideButtonOnOnClick;
+        }
+        private void hideButtonOnOnClick(object? sender, MouseButtonEventArgs e) {
+            Backend currentBackend = FurballGame.Instance.WindowManager.Backend;
+            EventLoop currentEventLoop = FurballGame.Instance.EventLoop;
+
+            FurballGame.Instance.WindowManager.Backend = Backend.Dummy;
+            FurballGame.Instance.RecreateWindow(new HeadlessEventLoop());
+
+            FurballGame.GameTimeScheduler.ScheduleMethod(delegate {
+                FurballGame.Instance.WindowManager.Backend = currentBackend;
+                FurballGame.Instance.RecreateWindow(currentEventLoop);
+            }, FurballGame.Time + 5000d);
         }
     }
 }
