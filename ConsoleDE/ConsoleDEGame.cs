f3b94fc616e6c2eb9ec1d99b4dc279b6cc532e61
jvyden <jvyden@jvyden.xyz>
2022-09-21 11:02:29 -0400

    Control updaterate and framerate when unfocused

--- ConsoleDE/ConsoleDEGame.cs
+++ ConsoleDE/ConsoleDEGame.cs
@@ -8,27 +8,33 @@ namespace ConsoleDE {
     public class ConsoleDEGame : FurballGame {
         public ConsoleDEGame() : base(new MainScreen()) {}
 
-        public static ConsoleDEGame Instance => (ConsoleDEGame)FurballGame.Instance;
+        public new static ConsoleDEGame Instance => (ConsoleDEGame)FurballGame.Instance;
 
         private ColorPalette palette = Palettes.Light;
         public ColorPalette CurrentPalette {
-            get {
-                return this.palette;
-            }
+            get => this.palette;
             set {
                 this.palette = value;
                 (this.RunningScreen as ConsoleScreen)?.UpdateColors();
             }
         }
 
-        public void Run(WindowOptions options) {
-            base.Run(options);
-        }
+        /// <summary>
+        /// Run the game with custom window options.
+        /// </summary>
+        /// <param name="options">The <see cref="WindowOptions"/> to start up with.</param>
+        public void Run(WindowOptions options) => base.Run(options); // expose protected function
 
         protected override void Initialize() {
             base.Initialize();
-            
+
+            // When unfocused, limit to 30fps (debug) or 1fps (release).
+            #if DEBUG
             this.WindowManager.TargetUnfocusedFramerate = 30;
+            #else
+            this.WindowManager.TargetUnfocusedFramerate = 1;
+            #endif
+            
             this.WindowManager.TargetUnfocusedUpdaterate = 30;
         }
 
--- ConsoleDE/Program.cs
+++ ConsoleDE/Program.cs
@@ -3,10 +3,16 @@ using Silk.NET.Windowing;
 namespace ConsoleDE {
     internal static class Program {
         internal static void Main(string[] args) {
+            WindowOptions options = WindowOptions.Default;
+            
             // We don't need low latency and high framerates.
             // Just stick to what the display is capable of.
-            WindowOptions options = WindowOptions.Default;
-            options.VSync = true; 
+            options.VSync = true;
+            
+            // On a developer system, fullscreen would get in the way.
+            // The intended use-case is being a "desktop environment" of sorts, on a dedicated computer.
+            //
+            // Thus, we only fullscreen on release builds.
             #if !DEBUG
             options.WindowState = WindowState.Fullscreen;
             options.WindowBorder = WindowBorder.Hidden;
