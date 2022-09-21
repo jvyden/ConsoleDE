62d8566426a01a589ea26fbe15ebf59a6031e2b1
jvyden <jvyden@jvyden.xyz>
2022-09-21 11:55:31 -0400

    Give all palettes better colors

--- ConsoleDE/Base/Palettes.cs
+++ ConsoleDE/Base/Palettes.cs
@@ -9,22 +9,32 @@ namespace ConsoleDE.Base {
     [SuppressMessage("ReSharper", "UnusedMember.Global")]
     public static class Palettes {
         public static readonly ColorPalette Light = new() {
-            PrimaryColor = Color.White,
-            SecondaryColor = Color.LightGray,
+            PrimaryColor = Color.LightGray,
+            SecondaryColor = Color.DimGray,
             TextColor = Color.Black,
-            BackgroundColor = Color.DarkRed,
+            BackgroundColor = Color.White,
         };
         
-        public static readonly ColorPalette Dark = new() {
-            PrimaryColor = Color.Black,
-            SecondaryColor = Color.DimGray,
+        public static readonly ColorPalette Steel = new() {
+            PrimaryColor = new Color(10, 9, 12),
+            SecondaryColor = new Color(52, 49, 63),
             TextColor = Color.White,
+            BackgroundColor = new Color(27, 25, 33),
         };
         
         public static readonly ColorPalette Purple = new() {
-            PrimaryColor = new Color(110,101,168),
-            SecondaryColor = new Color(127,116,194),
-            TextColor = new Color(246,245,252),
+            PrimaryColor = new Color(112, 99, 198),
+            SecondaryColor = new Color(127, 116, 194),
+            TextColor = new Color(246, 245, 252),
+            BackgroundColor = new Color(110, 101, 168),
+        };
+
+        public static readonly ColorPalette Hack = new() {
+            PrimaryColor = new Color(21, 178, 21),
+//            SecondaryColor = new Color(96, 96, 96),
+            SecondaryColor = new Color(33, 237, 33),
+            TextColor = new Color(33, 237, 33),
+            BackgroundColor = Color.Black,
         };
 
         #region Palette List
